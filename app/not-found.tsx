2a92e3b9498e522480b61a5ae580761f6a188b39
hweinian <hweinian@gmail.com>
2024-05-19 20:28:28 +0800

    Added Login Page

---
+++ app/not-found.tsx
@@ -0,0 +1,15 @@
+import Image from 'next/image';
+
+export default function NotFound() {
+    return (
+        <div>
+            <h1>Error 404: Page Not Found</h1>
+            <Image 
+            src="/notfound404.png"
+            alt="Picture of Kafka"
+            width={300}
+            height={300}
+            />
+        </div>
+    )
+}
