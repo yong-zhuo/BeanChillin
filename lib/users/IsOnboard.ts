4725f4fee29d289e741bb2fe70c2a9ed5e3e4224
hweinian <hweinian@gmail.com>
2024-06-02 23:01:53 +0800

    added loading screen

--- lib/users/IsOnboard.ts
+++ lib/users/IsOnboard.ts
@@ -2,18 +2,18 @@
 import prisma from "../prisma";
 import { getServerSession } from "next-auth";
 import { Oauth } from "./OAuth";
-import { redirect } from "next/navigation";
 
 export default async function IsOnboard() {
     //reroute user to home if session detected
     const ServerSession = await getServerSession(Oauth);
     if (ServerSession === null) {
-        redirect('/login');
+        return 'login';
     }
     const email = ServerSession?.user ? ServerSession.user.email : null;
     //in the event of error
     if (email === null) {
-        redirect('/register');
+        return 'register';
+
     }
     const res = await prisma.user.findFirst({
         where: {
@@ -23,5 +23,5 @@ export default async function IsOnboard() {
             isOnboard: true,
         }
     });
-    return res === null ? null : res.isOnboard ? redirect('/home') : null;
+    return res === null ? 'ok' : res.isOnboard ? 'home' : 'ok';
 }
