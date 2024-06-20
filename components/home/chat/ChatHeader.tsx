346450f1a6576c40fd0bd1707087147a4721adab
yong-zhuo <yap.aloysious@gmail.com>
2024-06-20 22:47:02 +0800

    Update chat header

--- components/home/chat/ChatHeader.tsx
+++ components/home/chat/ChatHeader.tsx
@@ -1,38 +1,34 @@
-
+import UserAvatar from "@/components/common-ui/misc/UserAvatar";
 import Image from "next/image";
 
 interface ChatHeaderProps {
-    name: string | null;
-    imageUrl: string | null;
+  name: string | null;
+  imageUrl: string | null;
 }
 
 export default function ChatHeader({ params }: { params: ChatHeaderProps }) {
-    const { name, imageUrl } = params;
+  const { name, imageUrl } = params;
 
-    return (
-        <div className="flex-1 justify-between flex flex-col h-full max-h-[calc(100vh-6rem)] ">
-            <div className='flex sm:items-center justify-between py-3 border-b-2 border-gray-200 bg-pri px-6 rounded-t-md'>
-                <div className='relative flex items-center space-x-4'>
-                    <div className='relative'>
-                        <div className='relative w-8 sm:w-12 h-8 sm:h-12'>
-                            <Image
-                                src={imageUrl === null ? "/profile/default.jpg" : imageUrl}
-                                alt={`${name} profile picture`}
-                                className='rounded-full'
-                                width={50}
-                                height={50}
-                            />
-                        </div>
-                    </div>
-
-                    <div className="flex flex-col leading-tight">
-                        <div className="text-xl flex items-center">
-                            <span className='text-white mr-3 font-semibold'>{name}</span>
-                        </div>
-                    </div>
-                </div>
+  return (
+    <div className="flex h-full max-h-[calc(100vh-6rem)] flex-1 flex-col justify-between ">
+      <div className="flex justify-between rounded-t-md border-b-2 border-gray-200 bg-pri px-6 py-3 sm:items-center">
+        <div className="relative flex items-center space-x-4">
+          <div className="relative">
+            <div className="relative h-8 w-8 sm:h-12 sm:w-12 items-center flex justify-center">
+              <UserAvatar
+                user={{ name: name || null, imageUrl: imageUrl || null }}
+                className="rounded-full"
+              />
             </div>
-        </div>
-    )
+          </div>
 
+          <div className="flex flex-col leading-tight">
+            <div className="flex items-center text-xl">
+              <span className="mr-3 font-semibold text-white">{name}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
