b0a4f48fca4679527bcef2fba4bbe3805425e28c
hweinian <hweinian@gmail.com>
2024-06-28 00:05:12 +0800

    disabled linter for signup.test.tsx

--- components/user-auth/register/signup.test.tsx
+++ components/user-auth/register/signup.test.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 import { fireEvent, render, screen, waitFor } from '@testing-library/react';
-import SignUpForm from './SignUpForm'; 
+import SignUpForm from './SignUpForm';
 import "@testing-library/jest-dom/";
 import { useRouter } from 'next/navigation';
 import CreateAccount from '@/lib/users/CreateAccount';
 import { signIn } from 'next-auth/react';
 
 jest.mock('next/navigation', () => ({
-    useRouter: jest.fn(),
-  }));
+  useRouter: jest.fn(),
+}));
 
 
 describe('SignUpForm', () => {
   test('renders SignUpForm component', () => {
     (useRouter as jest.Mock).mockReturnValue({
-        push: jest.fn(),
-      });
+      push: jest.fn(),
+    });
     render(<SignUpForm />);
-    
-  
+
+
     expect(screen.getByText('Create an account')).toBeInTheDocument();
     expect(screen.getByRole('link', { name: 'Login Now' })).toBeInTheDocument();
     expect(screen.getByRole('button', { name: 'Create Account' })).toBeInTheDocument();
@@ -41,8 +41,9 @@ describe('SignUpForm', () => {
     await waitFor(() => {
       expect(screen.getByText('Email is required')).toBeInTheDocument();
       const passwordErrors = screen.getAllByText('At least 8 characters long');
+      // eslint-disable-next-line testing-library/no-wait-for-multiple-assertions
       expect(passwordErrors).toHaveLength(2);
     });
-   
+
   });
 });
