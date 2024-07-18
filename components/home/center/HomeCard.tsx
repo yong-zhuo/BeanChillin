c580a32632134cfb8dfbfd1425ed8454e6a7390b
hweinian <hweinian@gmail.com>
2024-07-18 08:32:55 +0800

    updated suggested friends/groups/posts

--- components/home/center/HomeCard.tsx
+++ components/home/center/HomeCard.tsx
@@ -5,23 +5,27 @@ import {
   CardTitle,
 } from "@/components/common-ui/shadcn-ui/card";
 import { Bean } from "lucide-react";
+import HomeFeed from "../feed/HomeFeed";
 
 const HomeCard = () => {
   return (
-    <Card className="mt-7 h-full">
-      <CardContent className="mt-3 flex flex-col justify-center">
-        <CardHeader className="flex justify-center items-center">
-          <Bean size={50} color="#4D869C" />
-        </CardHeader>
-        <CardTitle className="text-center text-2xl font-semibold">
-          Welcome to BeanChillin
-        </CardTitle>
-        <p className="text-center text-zinc-500">
-          BeanChillin is a platform for discussing topics and meeting new friends. Create
-          groups, share memories and stay connected.
-        </p>
-      </CardContent>
-    </Card>
+    <div>
+      <Card className="mt-7 h-full mb-5">
+        <CardContent className="mt-3 flex flex-col justify-center">
+          <CardHeader className="flex justify-center items-center">
+            <Bean size={50} color="#4D869C" />
+          </CardHeader>
+          <CardTitle className="text-center text-2xl font-semibold">
+            Welcome to BeanChillin
+          </CardTitle>
+          <p className="text-center text-zinc-500">
+            BeanChillin is a platform for discussing topics and meeting new friends. Create
+            groups, share memories and stay connected.
+          </p>
+        </CardContent>
+      </Card>
+      <HomeFeed />
+    </div>
   );
 };
 
